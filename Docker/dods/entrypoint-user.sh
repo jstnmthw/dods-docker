7830223ab5caaf14815ff182aa0fbecd90954d53
Justin <web@jstn.ly>
2023-10-22 02:52:44 +0700

    Condition variable index should be true for remote cfg downloads

--- Docker/dods/entrypoint-user.sh
+++ Docker/dods/entrypoint-user.sh
@@ -87,7 +87,7 @@ if [ -z "$(ls -A -- "/data/serverfiles/${FOLDERNAME}/addons/rcbot2" 2> /dev/null
 fi
 
 # Download remote cfg files
-if [ "${install}" == 0 ]; then
+if [ "${install}" == 1 ]; then
   echo -e ""
   echo -e "Downloading remote cfg files"
   echo -e "================================="
